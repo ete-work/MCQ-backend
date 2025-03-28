04a62b21b0730d21a413b5d35ab1f7017f1f7087
etedigital <etedevelopment@gmail.com>
2025-03-28 16:19:30 +0530

    commit

--- server.js
+++ server.js
@@ -18,37 +18,7 @@ connectDB();
 const app = express();
 
 //Middleware
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://mcq-frontend-bice.vercel.app",
-  "https://whimsical-florentine-d89544.netlify.app",
-];
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("Incoming Origin:", origin); // Debugging log
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  credentials: true,
-  optionsSuccessStatus: 200,
-};
-
-// Apply CORS before routes
-app.use(cors(corsOptions));
-
-// Logging middleware
-app.use((req, res, next) => {
-  console.log(`Incoming request: ${req.method} ${req.path}`);
-  next();
-});
-
-// Ensure JSON parsing
+app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 
 //Setting Routes
